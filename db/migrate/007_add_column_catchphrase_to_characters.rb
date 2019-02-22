509e4ac071b1c5d39b592299d41430cca349f6c6
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 23:45:01 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_column_catchphrase_to_characters.rb
+++ db/migrate/007_add_column_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
-class AddColumnCatchphraseToCharacters < ActiveRecord::Migration 
+class AddColumnCatchphraseToCharacters < ActiveRecord::Migration
   def change
-    add_column :characters, :catchphrase, :string 
-  end 
+    add_column :characters, :catchphrase, :string
+  end
 end 
